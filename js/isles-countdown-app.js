dd9c7a1032c43b7ee373a033a5c8b74a726ee5ee
chadgmoore <smap2d@gmail.com>
2017-08-03 16:49:16 -0400

    updated roster, and 2017 season start date (2017/10/6

--- js/isles-countdown-app.js
+++ js/isles-countdown-app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var seasonStartDate="2016/10/13";
+    var seasonStartDate="2017/10/6";
 
     var oneDay = 24*60*60*1000;
     var todaysDate = new Date();
@@ -14,23 +14,25 @@ $(document).ready(function() {
     var data = {"islandersRoster":[
     {"sweaterNumber":"1", "firstName":"Thomas", "lastName":"Greiss", "position":"G", "gif":"http://bloguin.com/islandersinsight/wp-content/uploads/sites/173/2014/10/Tavares-Goal-vs-Hurricanes.gif"},
     {"sweaterNumber":"2", "firstName":"Nick", "lastName":"Leddy", "position":"D", "gif":"http://i.imgur.com/bBiwzJY.gif"},
-    {"sweaterNumber":"3", "firstName":"Travis", "lastName":"Hamonic", "position":"D", "gif":"http://bloguin.com/islandersinsight/wp-content/uploads/sites/173/2014/11/Hamonic-Goal-vs-Capitals.gif"},
+    {"sweaterNumber":"6", "firstName":"Ryan", "lastName":"Pulock", "position":"D", "gif":"http://i.imgur.com/bBiwzJY.gif"},
+    {"sweaterNumber":"7", "firstName":"Jordan", "lastName":"Eberle", "position":"F", "gif":"http://i.imgur.com/bBiwzJY.gif"},
+    {"sweaterNumber":"10", "firstName":"Alan", "lastName":"Quine", "position":"C", "gif":"https://snyislanders.files.wordpress.com/2013/12/clutterbuckshhhh.gif"},
+    {"sweaterNumber":"11", "firstName":"Shane", "lastName":"Prince", "position":"LW", "gif":"https://snyislanders.files.wordpress.com/2013/12/clutterbuckshhhh.gif"},
     {"sweaterNumber":"12", "firstName":"Josh", "lastName":"Bailey", "position":"L", "gif":"http://i.imgur.com/OKwlm99.gif"},
     {"sweaterNumber":"14", "firstName":"Thomas", "lastName":"Hickey", "position":"D", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2014/01/Thomas-Hickey-Goal-vs-Rangers.gif"},
-    {"sweaterNumber":"15", "firstName":"Cal", "lastName":"Clutterbuck", "position":"R", "gif":"https://snyislanders.files.wordpress.com/2013/12/clutterbuckshhhh.gif"},
-    {"sweaterNumber":"17", "firstName":"Matt", "lastName":"Martin", "position":"L", "gif":"http://cdn2.sbnation.com/imported_assets/1598309/crosbyhit_medium.gif"},
-    {"sweaterNumber":"18", "firstName":"Ryan", "lastName":"Strome", "position":"C", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2014/03/Ryan-Strome-Goal-vs-Canucks.gif"},
-    {"sweaterNumber":"21", "firstName":"Kyle", "lastName":"Okposo", "position":"R", "gif":"http://assets.sbnation.com/assets/2579843/okposo.gif"},
+    {"sweaterNumber":"15", "firstName":"Cal", "lastName":"Clutterbuck", "position":"RW", "gif":"https://snyislanders.files.wordpress.com/2013/12/clutterbuckshhhh.gif"},
+    {"sweaterNumber":"16", "firstName":"Andrew", "lastName":"Ladd", "position":"LW", "gif":"https://snyislanders.files.wordpress.com/2013/12/clutterbuckshhhh.gif"},
+    {"sweaterNumber":"25", "firstName":"Jason", "lastName":"Chimera", "position":"LW", "gif":"http://giant.gfycat.com/AdvancedCourageousBarracuda.gif"},
     {"sweaterNumber":"27", "firstName":"Anders", "lastName":"Lee", "position":"C", "gif":"http://giant.gfycat.com/GraveBraveAntlion.gif"},
     {"sweaterNumber":"29", "firstName":"Brock", "lastName":"Nelson", "position":"C", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2014/02/Brock-Nelson-Goal-vs-Rangers.gif"},
-    {"sweaterNumber":"37", "firstName":"Brian", "lastName":"Strait", "position":"D", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2014/01/Brian-Stait-Goal-vs-Flyers.gif"},
-    {"sweaterNumber":"40", "firstName":"Michael", "lastName":"Grabner", "position":"R", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2013/12/Grabner-Goal-vs-Rangers.gif"},
     {"sweaterNumber":"41", "firstName":"Jaroslav", "lastName":"Halak", "position":"G", "gif":"https://media2.giphy.com/media/91uSZRRHVszCM/200.gif"},
-    {"sweaterNumber":"44", "firstName":"Calvin", "lastName":"De Haan", "position":"D", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2014/02/Calvin-de-Haan-Goal-vs-Flames.gif"},
-    {"sweaterNumber":"51", "firstName":"Frans", "lastName":"Nielsen", "position":"C", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2014/03/Frans-Nielsen-Goal-vs-Oilers.gif"},
+    {"sweaterNumber":"42", "firstName":"Scott", "lastName":"Mayfield", "position":"D", "gif":"https://media2.giphy.com/media/91uSZRRHVszCM/200.gif"},
+    {"sweaterNumber":"44", "firstName":"Calvin", "lastName":"de Haan", "position":"D", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2014/02/Calvin-de-Haan-Goal-vs-Flames.gif"},
+    {"sweaterNumber":"50", "firstName":"Adam", "lastName":"Pelech", "position":"D", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2014/01/Casey-Cizikas-Goal-vs-Blackhawks.gif"},
     {"sweaterNumber":"53", "firstName":"Casey", "lastName":"Cizikas", "position":"C", "gif":"http://cdn.fansided.com/wp-content/blogs.dir/111/files/2014/01/Casey-Cizikas-Goal-vs-Blackhawks.gif"},
     {"sweaterNumber":"55", "firstName":"Johnny", "lastName":"Boychuk", "position":"D", "gif":"http://giant.gfycat.com/AdvancedCourageousBarracuda.gif"},
-    {"sweaterNumber":"84", "firstName":"Mikhail", "lastName":"Grabovski", "position":"C", "gif":"http://giant.gfycat.com/HelplessSecretAidi.gif"},
+    {"sweaterNumber":"66", "firstName":"Joshua", "lastName":"Ho-Sang", "position":"RW", "gif":"http://giant.gfycat.com/AdvancedCourageousBarracuda.gif"},
+    {"sweaterNumber":"72", "firstName":"Anthony", "lastName":"Beauvillier", "position":"C", "gif":"http://giant.gfycat.com/AdvancedCourageousBarracuda.gif"},
     {"sweaterNumber":"86", "firstName":"Nikolay", "lastName":"Kulemin", "position":"L", "gif":"http://bloguin.com/islandersinsight/wp-content/uploads/sites/173/2014/11/Kulemin-Goal-vs-Penguins.gif"},
     {"sweaterNumber":"91", "firstName":"John", "lastName":"Tavares", "position":"C", "gif":"http://i.imgur.com/A1nf6XM.gif"}    
 ]};
